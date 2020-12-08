04700f00650e333eb8fca327cebc619b2e8319cd
jared Long <jlong4223@gmail.com>
2020-12-07 22:03:19 -0600

    requires and uses route for json and creates bike w/playground

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 require('./config/database')
 
 // TODO require routes 
-
+const bikesRouter = require('./routes/bikes')
 
 // running express 
 const app = express()
@@ -19,7 +19,21 @@ app.use(cors())
 app.use(logger('dev'))
 
 // TODO use routes here 
+app.use('/', bikesRouter)
 
+// // playground below 
+// const Bike = require('./models/bike')
+// Bike.create({
+//     name: 'X-Caliber',
+//     type: 'Mountain/XC', 
+//     location: 'Austin, TX 78745',
+//     brand: 'Trek', 
+//     size: 'Adult Lg',
+//     availableTill: 'December 1, 2020',
+//     imgURL: 'https://i.imgur.com/QmOwXx9.jpg'
+// }, function(err,doc){
+//     console.log(doc)
+// })
 
 // defining and running the app
 const port = process.env.PORT || 3001
