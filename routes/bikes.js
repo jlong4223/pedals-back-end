d051ec08d7fabcc141dc4248dc427caa335c28eb
jared Long <jlong4223@gmail.com>
2020-12-08 12:29:05 -0600

    updates notes for later

--- routes/bikes.js
+++ routes/bikes.js
@@ -3,14 +3,18 @@ const router = express.Router()
 // defined the bikesctrl in the controller
 const bikesCtrl = require('../controllers/bikes')
 
+//read routes here
 router.get('/', bikesCtrl.showBikes)
+
 /*---------- Protected Routes ----------*/
 // Process the token for only the routes below
+//CUD routes here (create, update, delete)
 router.use(require('../config/auth'));
 // adds check auth so if user isnt signed in or token is not valid, gives not authorized err
 router.post('/', checkAuth, bikesCtrl.create)
 
-//restrict access so only valid authed users can access route
+//restrict access so only valid authed users can access route- 
+// add this function to any routes that you want protected by auth
 function checkAuth(req, res, next){
     if (req.user) return next()
     return res.status(401).json({msg: 'not authorized'})
