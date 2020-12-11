dd553637e23128d6c8cf81174c3eaa7946be53c8
jared Long <jlong4223@gmail.com>
2020-12-10 22:45:38 -0600

    removes checkAuth from a route

--- routes/bikes.js
+++ routes/bikes.js
@@ -5,13 +5,15 @@ const bikesCtrl = require('../controllers/bikes')
 
 // TODO read routes here
 router.get('/', bikesCtrl.showBikes)
+router.post('/', bikesCtrl.create)
 
 /*---------- Protected Routes ----------*/
 // Process the token for only the routes below
 // TODO CUD routes here (create, update, delete)
 router.use(require('../config/auth'));
 // adds check auth so if user isnt signed in or token is not valid, gives not authorized err
-router.post('/', checkAuth, bikesCtrl.create)
+// TODO if you want create post function to be used by only logged in users, do the following:
+// router.post('/', checkAuth, bikesCtrl.create)
 
 //restrict access so only valid authed users can access route- 
 // TODO add this function to any routes that you want protected by auth
