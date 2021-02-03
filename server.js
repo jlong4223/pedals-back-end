a3af8388c7d3ee1422e6e1ca91494b3329b02cd6
Jared Long <jlong4223@gmail.com>
2021-02-02 22:04:52 -0600

    uses welcome ctrl and route

--- server.js
+++ server.js
@@ -1,45 +1,32 @@
 // requiring the modules
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-// running express 
-const app = express()
+// running express
+const app = express();
 
 // requires .env file and require DB module
-require('dotenv').config()
-require('./config/database')
+require("dotenv").config();
+require("./config/database");
 
-// TODO require routes 
-const bikesRouter = require('./routes/bikes')
-const usersRouter = require('./routes/users')
-
+// TODO require routes
+const bikesRouter = require("./routes/bikes");
+const usersRouter = require("./routes/users");
+const welcomeRouter = require("./routes/welcome");
 
 // middleware being used
-app.use(express.json())
-app.use(cors())
-app.use(logger('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
 
-// TODO use routes here 
-app.use('/bikes', bikesRouter)
-app.use('/users', usersRouter)
-
-// //======== playground below ========= // 
-// const Bike = require('./models/bike')
-// Bike.create({
-//     name: 'X-Caliber',
-//     type: 'Mountain/XC', 
-//     location: 'Austin, TX 78745',
-//     brand: 'Trek', 
-//     size: 'Adult Lg',
-//     availableTill: 'December 1, 2020',
-//     imgURL: 'https://i.imgur.com/QmOwXx9.jpg'
-// }, function(err,doc){
-//     console.log(doc)
-// })
+// TODO use routes here
+app.use("/bikes", bikesRouter);
+app.use("/users", usersRouter);
+app.use("/", welcomeRouter);
 
 // defining and running the app
-const port = process.env.PORT || 3001
-app.listen(port, function(){
-    console.log(`Express is running on port: ${port}`)
-})
+const port = process.env.PORT || 3001;
+app.listen(port, function () {
+  console.log(`Express is running on port: ${port}`);
+});
