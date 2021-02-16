b6a79bc7feb73bac0c7d7ef886935f951c74109e
Jared <jlong4223@gmail.com>
2021-02-15 22:35:48 -0600

    sets findandmondify to false for db

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-
 // requiring mongoose to connect to MongoDB
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // connecting to mongoDB using mongoose
 // create database_uri
-mongoose.connect(process.env.DATABASE_URI,{
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 //Shortcut variable for mongoose connection
-const db = mongoose.connection
+const db = mongoose.connection;
 
 // Making sure to listen for the connection
-db.once('connected', ()=>{
-    console.log(`Connected to MongoDB on ${db.host}: ${db.port}`)
-})
+db.once("connected", () => {
+  console.log(`Connected to MongoDB on ${db.host}: ${db.port}`);
+});
