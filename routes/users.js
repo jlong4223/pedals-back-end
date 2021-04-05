8521c7923f327ed6c75c8f26c43a200b40f51c7b
Jared Long <jlong4223@gmail.com>
2021-04-05 15:38:58 -0500

    adds route to see users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const usersCtrl = require('../controllers/users');
+const usersCtrl = require("../controllers/users");
 
-router.post('/signup', usersCtrl.signup);
-router.post('/login', usersCtrl.login)
+router.get("/users", usersCtrl.getUsers);
+router.post("/signup", usersCtrl.signup);
+router.post("/login", usersCtrl.login);
 
 module.exports = router;
