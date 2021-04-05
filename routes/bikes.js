2fdbebd937a9324243609c6d3d6a89c20455e185
Jared Long <jlong4223@gmail.com>
2021-04-05 15:38:45 -0500

    adds checkAuth to crud routes

--- routes/bikes.js
+++ routes/bikes.js
@@ -3,23 +3,19 @@ const router = express.Router();
 // defined the bikesctrl in the controller
 const bikesCtrl = require("../controllers/bikes");
 
-// TODO read routes here
+// read routes here
 router.get("/", bikesCtrl.showBikes);
 router.get("/:id", bikesCtrl.showOneBike);
-router.post("/", bikesCtrl.create);
-router.delete("/:id", bikesCtrl.deleteBike);
-router.put("/:id", bikesCtrl.updateBike);
 
-/*---------- Protected Routes ----------*/
-// Process the token for only the routes below
-// TODO CUD routes here (create, update, delete)
+/*---------------- Protected Routes ----------------*/
+/*--- Process the token for only the routes below: adds check auth so if user isnt signed in or token is not valid, gives not authorized err ---*/
 router.use(require("../config/auth"));
-// adds check auth so if user isnt signed in or token is not valid, gives not authorized err
-// TODO if you want create post function to be used by only logged in users, do the following:
-// router.post('/', checkAuth, bikesCtrl.create)
 
-//restrict access so only valid authed users can access route-
-// TODO add this function to any routes that you want protected by auth
+router.post("/", checkAuth, bikesCtrl.create);
+router.delete("/:id", checkAuth, bikesCtrl.deleteBike);
+router.put("/:id", checkAuth, bikesCtrl.updateBike);
+
+/* restrict access so only valid authed users can access route - adding this function to any routes that you want protected by auth */
 function checkAuth(req, res, next) {
   if (req.user) return next();
   return res.status(401).json({ msg: "not authorized" });
